2faaadd6baf7e7cc101e449389d3dd83aa03eeca
KamenSentai <cvt.alain@gmail.com>
2019-08-04 22:50:03 +0200

    feat(): shoot lasers

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,9 +3,11 @@
 import '~/styles/index.scss'
 
 import Grabber from './components/Grabber'
+import Laser from './components/Laser'
 import Space from './components/Space'
 
 /* Initialization */
 
 new Grabber('.area').init()
+new Laser().init()
 new Space().init()
